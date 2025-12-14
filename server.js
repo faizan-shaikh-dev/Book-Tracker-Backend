1ca613ceafe278b6cdd91d844f4ae093f3534515
faizan-shaikh-dev <faizangulab4276@gmail.com>
2025-12-14 15:37:48 +0530

    mongodb connection

---
+++ server.js
@@ -0,0 +1,24 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
+import { connectDB } from "./config/db.js";
+
+const app = express();
+
+dotenv.config();
+//DataBase Connection
+connectDB();
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("server is on");
+});
+
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`http://localhost:${PORT}`);
+});
